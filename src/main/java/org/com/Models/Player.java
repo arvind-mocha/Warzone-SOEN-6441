cbba756104fdf5671d914f63286dcf93d8834652
Barath-S29 <barath.2952@gmail.com>
2025-02-16 18:34:17 -0500

    Updated Player.java with necessary attributes and constructors.

--- src/main/java/org/com/Models/Player.java
+++ src/main/java/org/com/Models/Player.java
@@ -1,6 +1,37 @@
 package org.com.Models;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This java file is used to define a player in the game
+ * Contains all the attributes of a player.
+ */
 
 public class Player {
 
-    private static String name;
+    private String d_name;
+    private int d_armyCount;
+    private List<Country>d_countries;   //Countries owned by a particular player
+    private List<Continent>d_continents;   //Continents owned by a particular player
+
+    /**
+     * This is a default constructor which is used in the beginning of the game to get player's name and
+     * assign random countries.
+     * @param p_name Input from user requesting the Player's name
+     */
+    public Player(String p_name){
+        this.d_name = p_name;
+        this.d_armyCount = 0;
+        this.d_countries = new ArrayList<>();
+        this.d_continents = new ArrayList<>();
+    }
+
+    /**
+     * When a player takes over a country, this method is used to add that country in their Country list.
+     * Can also be used to add country to player's list.
+     * @param p_country Country name
+     */
+    public void addCountry(Country p_country){
+        this.d_countries.add(p_country);
+    }
 }
