66e299f7e1328a315666ca4e6a17d3c8489f14d8
Barath-S29 <barath.2952@gmail.com>
2025-02-16 19:58:00 -0500

    Directory changes

--- src/main/java/org/com/GamePage/GameEngine.java
+++ src/main/java/org/com/GamePage/GameEngine.java
@@ -1,14 +1,20 @@
-package org.com.Controller;
+package org.com.GamePage;
 
 import org.com.Constants.CommonConstants;
 
+import java.util.Scanner;
 import java.io.Serializable;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class GameEngine implements Serializable {
     public static void main(String[] args) {
-            var console = System.console();
-            console.print(CommonConstants.WELCOME_MESSAGE);
+        Scanner l_scanner = new Scanner(System.in);
+
+        var console = System.console();
+        console.print(CommonConstants.WELCOME_MESSAGE);
+        System.out.println(CommonConstants.HELP_MESSAGE);
+
+
     }
 }
