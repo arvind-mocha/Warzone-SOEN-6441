47bce5c1596b23763872782a9c0eba520d981616
Swathi-Priya-P <pksp73.ca@gmail.com>
2025-03-03 21:44:58 -0500

    created LoadMapTest.java file

---
+++ src/test/java/controller/LoadMapTest.java
@@ -0,0 +1,25 @@
+package controller;
+
+import org.com.Models.Country;
+import org.com.Models.Map;
+import org.com.Models.Order;
+import org.com.Models.Player;
+//import org.com.Models.DeployOrder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.com.GamePhase.GameStartUpPhase;
+//import strategy.HumanStrategy;
+//import util.MapUtil;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoadMapTest {
+
+
+}
