4da734e59f134664e9081ef29a5c7883d7c32ba0
Arvind-mocha <arvindarvind2210@gmail.com>
2025-04-09 00:00:28 -0400

    Cheater

--- src/main/java/org/com/Strategies/CheaterStrategy.java
+++ src/main/java/org/com/Strategies/CheaterStrategy.java
@@ -1,5 +1,6 @@
 package org.com.Strategies;
 
+import org.com.GameLog.LogManager;
 import org.com.Handlers.GamePhaseHandler;
 import org.com.Models.Country;
 import org.com.Models.Map;
@@ -7,9 +8,30 @@ import org.com.Models.Player;
 
 import java.util.List;
 
-public class CheaterStrategy implements Strategy{
+public class CheaterStrategy implements Strategy {
     @Override
     public List<String> createOrder(GamePhaseHandler p_gamePhaseHandler, Player p_currentPlayer) {
+        Player l_currentPlayer = p_gamePhaseHandler.getPlayerList().get(p_gamePhaseHandler.getCurrentPlayer());
+        if (l_currentPlayer.get_countries().isEmpty()) return null;
+
+        for (Country l_country : l_currentPlayer.get_countries()) {
+            for (int l_neighbourCountryId : l_country.getNeighbourCountryIds()) {
+                Country l_neighbour = p_gamePhaseHandler.getGameMap().getCountryById(l_neighbourCountryId);
+                if (l_neighbour.getOwner() != l_currentPlayer)
+                    l_neighbour.getOwner().get_countries().remove(l_neighbour);
+                l_neighbour.setOwner(l_currentPlayer);
+                LogManager.logAction("CountryID " + l_neighbour.getId() + " conquered by " + l_currentPlayer.get_name());
+            }
+        }
+        for (Country l_country : l_currentPlayer.get_countries()) {
+            for (int l_neighbourCountryId : l_country.getNeighbourCountryIds()) {
+                Country l_neighbour = p_gamePhaseHandler.getGameMap().getCountryById(l_neighbourCountryId);
+                if (l_neighbour.getOwner() != l_currentPlayer) {
+                    l_country.setArmyCount(2 * l_country.getArmyCount());
+                    break;
+                }
+            }
+        }
         return null;
     }
 
