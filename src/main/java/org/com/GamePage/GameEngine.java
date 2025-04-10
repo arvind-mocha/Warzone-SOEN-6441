58c97b0a051ff63b7017046b2e16b47bdc0a5a92
Barath <barath.2952@gmail.com>
2025-04-09 21:28:31 -0400

    Refactoring

--- src/main/java/org/com/GamePage/GameEngine.java
+++ src/main/java/org/com/GamePage/GameEngine.java
@@ -6,19 +6,15 @@ import org.com.GamePhase.IssueOrderPhase;
 import org.com.Handlers.CommandHandler;
 import org.com.Handlers.GamePhaseHandler;
 import org.com.GameLog.LogManager;
-import org.com.Handlers.MapOperationsHandler;
-import org.com.Models.Country;
 import org.com.Models.Player;
 import org.com.Models.Tournament;
 import org.com.Strategies.HumanStrategy;
 import org.com.Strategies.Strategy;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 /**
  * The main game engine set of codes are present here.
@@ -36,9 +32,9 @@ public class GameEngine implements Serializable {
     public static void main(String[] p_args) {
 
         //The following messages will be displayed at the start of the game.
-        var l_console = System.console();
-        l_console.println("Welcome to the WarZone edition of Risk.");
-        l_console.println(CommandOutputMessages.HELP_DEFAULT_MESSAGE);
+//        var l_console = System.console();
+        System.out.println("Welcome to the WarZone edition of Risk.");
+        System.out.println(CommandOutputMessages.HELP_DEFAULT_MESSAGE);
 
 
         // Getting Input from the players
@@ -51,33 +47,9 @@ public class GameEngine implements Serializable {
             boolean l_isIssueOrderPhase = l_gamePhaseManager.getGamePhase() instanceof IssueOrderPhase;
             List<Player> l_gamePlayerList = l_gamePhaseManager.getPlayerList();
             Player l_currentPlayer = l_gamePlayerList.isEmpty() ? null : l_gamePlayerList.get(l_gamePhaseManager.getCurrentPlayer());
-            List<Player> l_ownersMap = new ArrayList<>();
-
-            if(l_gamePhaseManager.getGameMap() != null){
-                for (Country l_country : l_gamePhaseManager.getGameMap().getCountryMap().vertexSet()){
-                    if(l_country.getOwner() != null) {
-                        l_ownersMap.add(l_country.getOwner());
-                    }
-                }
-                l_ownersMap = l_ownersMap.stream().distinct().collect(Collectors.toList());
-//                System.out.println(l_ownersMap.size());
-            }
-
-
-//            if(l_isIssueOrderPhase && l_currentPlayer.get_countries().size() == l_gamePhaseManager.getGameMap().getCountryMap().vertexSet().size())
-//            if(l_isIssueOrderPhase && l_gamePhaseManager.getTurnsCompleted() > 400)
-            if(l_isIssueOrderPhase && l_ownersMap.size() <= 1)
+            if(l_isIssueOrderPhase && l_currentPlayer.get_countries().size() == l_gamePhaseManager.getGameMap().getCountryMap().vertexSet().size())
             {
-                Player l_winner = l_ownersMap.getFirst();
-                l_console.println(String.format("%d, %d", l_winner.get_countries().size(), l_gamePhaseManager.getGameMap().getCountryMap().vertexSet().size()));
-                l_console.println(String.format("Hurray!!!. Player %s won the game.", l_winner.get_name()));
-
-                try{
-                    MapOperationsHandler.processShowGameMap(l_gamePhaseManager);
-                } catch (Exception e){
-                    System.out.println(e.toString());
-                }
-
+                System.out.println(String.format("Hurray!!!. Player %s won the game.", l_currentPlayer.get_name()));
                 l_inputCommand = Arrays.asList(CommonConstants.EXIT_COMMAND);
             }
             else if(l_isIssueOrderPhase && l_currentPlayer.get_countries().isEmpty())
@@ -87,27 +59,23 @@ public class GameEngine implements Serializable {
             else if (l_isIssueOrderPhase && !(l_currentPlayer.get_playerStrategy() instanceof HumanStrategy))
             {
                 Strategy l_playerStrategy = l_currentPlayer.get_playerStrategy();
-//                l_console.println(String.format("%d, %d", l_currentPlayer.get_countries().size(), l_gamePhaseManager.getGameMap().getCountryMap().vertexSet().size()));
                 l_inputCommand = l_playerStrategy.createOrder(l_gamePhaseManager, l_currentPlayer);
             }
-
             else
             {
-                l_console.print("> ");
+                System.out.print("> ");
                 l_inputCommand = Arrays.asList(l_scanner.nextLine());
             }
-
-
             try {
                 CommandHandler.processCommand(l_gamePhaseManager, l_inputCommand);
             } catch (Exception e) {
-                l_console.println("\u001B[31m-- " + e.getMessage() + " --\u001B[0m");
+                System.out.println("\u001B[31m-- " + e.getMessage() + " --\u001B[0m");
                 LogManager.logAction("\u001B[31m-- " + e.getMessage() + " --\u001B[0m");
-                l_console.println(CommandOutputMessages.HELP_DEFAULT_MESSAGE);
+                System.out.println(CommandOutputMessages.HELP_DEFAULT_MESSAGE);
             }
         } while(l_inputCommand == null || !l_inputCommand.contains(CommonConstants.EXIT_COMMAND));
 
         LogManager.logAction("Game has been ended");
-        l_console.println("Thanks for giving our game a try! We hope you have an epic time on the battlefield. \uD83D\uDE80\uD83D\uDD25");
+        System.out.println("Thanks for giving our game a try! We hope you have an epic time on the battlefield. \uD83D\uDE80\uD83D\uDD25");
     }
 }
